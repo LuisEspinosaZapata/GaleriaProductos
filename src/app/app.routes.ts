55416f28b3fc5525faaaf09ed853c8dff26ef012
LuisEspinosaZapata <159159Papa>
2025-08-01 21:17:22 -0500

    Subir proyecto Angular completo

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProductosComponent } from './components/productos/productos.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'productos', component: ProductosComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
